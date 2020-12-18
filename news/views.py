14f8dc8cb2fe270a248066d42b36d85ba7fedbb7
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-12-18 10:38:00 +0300

    add media

--- news/views.py
+++ news/views.py
@@ -2,30 +2,9 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from django.db import models
 
 # Create your views here.
-# def welcome(request):
-#     return render(request, 'welcome.html')
-
-# def news_today(request):
-#     date = dt.date.today()
-#     return render(request, 'all-news/today-news.html', {"date": date,})
-
-# def past_days_news(request, past_date):
-
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-#         assert False
-
-#     if date == dt.date.today():
-#         return redirect(news_today)
-
-#     return render(request, 'all-news/past-news.html', {"date": date})
 
 def news_today(request):
     date = dt.date.today()
